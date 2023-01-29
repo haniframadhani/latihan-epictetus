5f12daa5f4f0cdb67baf66ade99308b5131102d4
haniframadhani <mhaniframadhani985@gmail.com>
2023-01-29 15:15:43 +0700

    json server dan target blank di link ke github dan title di halaman kategori

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,6 @@ import { Copy } from "react-feather";
 
 export default function Footer() {
   return (<footer className="text-white text-center text-base pb-16">
-    <p>Copyright &copy; 2021 - Design By {<Link href="#" className="hover:underline">Nauval Azhar</Link>}, Developed By {<Link href="#" className="hover:underline">Hanif Ramadhani</Link>}</p>
+    <p>Copyright &copy; 2021 - Design By {<Link href="https://github.com/nauvalazhar" className="hover:underline" target={'_blank'}>Nauval Azhar</Link>}, Developed By {<Link href="https://github.com/haniframadhani" className="hover:underline" target={'_blank'}>Hanif Ramadhani</Link>}</p>
   </footer>)
 }
--- pages/culinary/index.js
+++ pages/culinary/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Card from '../../components/Card';
-export default function Culinary({ blogs, authors }) {
+export default function Nature({ blogs, authors }) {
   return (
     <>
       <Head>
@@ -9,19 +9,22 @@ export default function Culinary({ blogs, authors }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="px-10 sm:px-20 md:px-28 lg:px-44 scroll-smooth py-10 text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-14">
-        {
-          blogs.map(blog => {
-            let au = null;
-            authors.map(author => {
-              if (blog["id-author"] === author.id) {
-                au = author;
-              }
-              return au;
+      <div className='px-10 sm:px-20 md:px-28 lg:px-44 scroll-smooth py-10 text-white '>
+        <h1 className='text-center text-3xl mb-24 capitalize leading-7'>{blogs[0]["category"]}</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-14">
+          {
+            blogs.map(blog => {
+              let au = null;
+              authors.map(author => {
+                if (blog["id-author"] === author.id) {
+                  au = author;
+                }
+                return au;
+              })
+              return <Card key={blog["id-post"]} blog={blog} author={au} index={null}></Card>
             })
-            return <Card key={blog["id-post"]} blog={blog} author={au} index={null}></Card>
-          })
-        }
+          }
+        </div>
       </div>
     </>
   )
